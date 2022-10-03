312c4589ae89667909bb3859f0d4b4b0dbfb6602
Aina <ainamoreno@outlook.com>
2022-10-03 15:57:27 +0200

    Realización del reto 1

---
+++ reto-1/js/main.js
@@ -0,0 +1,10 @@
+let numero1 = 70;
+let numero2 = 65;
+
+if (numero1 == numero2) {
+    console.log("Ambos números son iguales")
+} else if (numero1 > numero2) {
+    console.log("El número mayor es " + numero1)
+} else {
+    console.log("El número menor es " + numero1)
+};
